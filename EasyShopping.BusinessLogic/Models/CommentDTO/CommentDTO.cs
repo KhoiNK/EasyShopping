f21378356c2fedcf0c7e3a252d1ad1f9fdca70fe
Thien <Thien@THIENTDSE61408>
2017-04-06 02:38:47 +0700

    06/04/2017 AddCommentDTO , CommentTranslator (has bug) Rename Disctrict -> District

---
+++ EasyShopping.BusinessLogic/Models/CommentDTO/CommentDTO.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyShopping.BusinessLogic.Models
+{
+    public class CommentDTO
+    {
+        public int Id { get; set; }
+        public Nullable<string> Description { get; set; }
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public Nullable<int> ParentCmt { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
