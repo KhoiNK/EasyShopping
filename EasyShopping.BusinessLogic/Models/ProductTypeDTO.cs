3c875a158be594f2e7288feb8efced94f6cad700
Thien <Thien@THIENTDSE61408>
2017-05-13 14:37:40 +0700

    Dien het DTO

---
+++ EasyShopping.BusinessLogic/Models/ProductTypeDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyShopping.BusinessLogic.Models
+{
+    public class ProductTypeDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> CateID { get; set; }
+    }
+}
---
+++ EasyShopping.BusinessLogic/Models/WishlistDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyShopping.BusinessLogic.Models
+{
+    public class WishlistDTO
+    {
+        public int ID { get; set; }
+        public int ProductDetailID { get; set; }
+        public int UserID { get; set; }
+    }
+}
